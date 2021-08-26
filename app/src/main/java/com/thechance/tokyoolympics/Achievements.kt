288e482d9595a06834cfe0108e005c1bb0b85dec
Rafal A <rafal.akeel99@gmail.com>
2021-08-26 14:05:41 +0300

    add and parse csv file

---
+++ app/src/main/java/com/thechance/tokyoolympics/Achievements.kt
@@ -0,0 +1,9 @@
+package com.thechance.tokyoolympics
+
+data class Achievements(   val rank:Int,
+                      val team :String,
+                      val goldMedal:Int,
+                      val silverMedal:Int,
+                      val bronzeMedal:Int,
+                      val total:Int,
+                      val rankByTotal:Int)
--- app/src/main/java/com/thechance/tokyoolympics/MainActivity.kt
+++ app/src/main/java/com/thechance/tokyoolympics/MainActivity.kt
@@ -2,10 +2,32 @@ package com.thechance.tokyoolympics
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.thechance.tokyoolympics.databinding.ActivityMainBinding
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    private val LOG_TAG="Main_Activity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        parseCsvFile()
+    }
+
+    private fun parseCsvFile() {
+        //  will return inputStream and save it in buffer
+        val inputStream = assets.open("tokyo_2021.csv")
+        val parser = CsvParser()
+        /*
+        * buffer reader take stream reader as it's argument
+        * input stream reader takes input stream as it's argument*/
+        val buffer = BufferedReader(InputStreamReader(inputStream))
+        /*lambda function that uses log to run the app in log and see the info in the csv file */
+        buffer.forEachLine {
+            Log.v(LOG_TAG, it)
+        }
     }
 }
